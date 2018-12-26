5dea6cf77c120f3ef408931ea1b874d92a0c79cb
christinajoice <christinallrejoice@gmail.com>
2018-12-26 09:57:14 -0800

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Max
+{
+public static void main(String args[])
+{
+
+System.out.println("Enter the number of elements in an array");
+Scanner s=new Scanner(System.in);
+int N=s.nextInt();
+
+}
+}
